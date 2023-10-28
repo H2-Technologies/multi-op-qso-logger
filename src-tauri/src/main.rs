97859e92f47f90e7763062ab560815a182731bd5
ahadley1124 <admin@austinh.dev>
2023-10-28 12:50:52 -0400

    Add Rust Updater Code
    Signed-off-by: ahadley1124 <admin@austinh.dev>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -29,6 +29,11 @@ fn main() {
         .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init())
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
