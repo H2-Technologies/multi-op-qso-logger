b726ea3c58cf008ca9e9bb1e94234487161b5653
ahadley1124 <admin@austinh.dev>
2023-10-29 10:34:39 -0400

    Add a Version Script
    Signed-off-by: ahadley1124 <admin@austinh.dev>

---
+++ version.js
@@ -0,0 +1,8 @@
+//open the src-tauri/tauri.conf.json file and read the 0.package.version value and print it to the console
+
+const fs = require('fs');
+const path = require('path');
+
+const tauriConfPath = path.join(__dirname, './src-tauri/tauri.conf.json');
+const tauriConf = JSON.parse(fs.readFileSync(tauriConfPath, 'utf8'));
+console.log(tauriConf.package.version);
