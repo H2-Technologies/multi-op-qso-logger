e1489852163d7175eca146bf99f0713488ffc1a3
Austin Hadley <admin@austinh.dev>
2023-10-30 22:22:58 -0400

    Update code for Discord Help
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use serde::{Serialize, Deserialize};
+use tauri_plugin_updater::UpdaterExt;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,7 +10,7 @@ fn greet(name: &str) -> String {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct qso {
+pub struct Qso {
     callsign: String,
     frequency: u16,
     mode: String,
@@ -19,10 +20,6 @@ pub struct qso {
     comment: String
 }
 
-#[tauri::command]
-async fn command_name() -> Result<(), String> {
-  Ok(())
-}
 //invoke("qso_vec", {callsign: "KE8YGW", frequency: 14.255, mode: "SSB", rst_sent: 59, rst_recieved: 59, operator: "KE8YGW", comment: "OHIO"});
 fn main() {
     tauri::Builder::default()
@@ -33,7 +30,7 @@ fn main() {
         .setup(|app| {
             let handle = app.handle();
             tauri::async_runtime::spawn(async move {
-                let response = handle.updater().check().await;
+                let _response = handle.updater().expect("REASON").check().await;
             });
             Ok(())
         })
