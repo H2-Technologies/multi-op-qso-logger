c0e7c463d0b4aa9b269bf93b7b7f8de52a204f0f
ahadley1124 <admin@austinh.dev>
2023-10-30 23:49:41 -0400

    Remove SQL plugin for now
    Signed-off-by: ahadley1124 <admin@austinh.dev>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -26,7 +26,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_sql::Builder::default().build())
+        //.plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             let handle = app.handle().clone();
