7e898db711fe0fc0de3c7a2cd622600679110b3f
Austin Hadley <admin@austinh.dev>
2023-11-14 14:08:14 -0500

    Create Div for CSS Style
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src/window.js
+++ src/window.js
@@ -1,3 +1,3 @@
 const windowLink = window.__TAURI__.window;
 
-new windowLink.Window("Setup", { url: "setup.html", width: 400, height: 400});
+new windowLink.Window("Setup", { url: "setup.html", width: 750, height: 500});
