6fee4007b3c731a9abd06aae8bab243cc74e9ec1
Austin Hadley <admin@austinh.dev>
2023-11-17 21:48:00 -0500

    Update Depencies
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::http::response;
 use tauri_plugin_updater::UpdaterExt;
+use std::time::Duration;
+use std::thread::sleep;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
