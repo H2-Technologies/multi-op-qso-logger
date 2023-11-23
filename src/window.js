746a12c266b545bb3089550985820463356e9879
Austin Hadley <admin@austinh.dev>
2023-11-22 20:04:45 -0500

    Fix Workflows from Conflicting
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src/window.js
+++ src/window.js
@@ -1,3 +1,11 @@
 const windowLink = window.__TAURI__.window;
+let invoke = window.__TAURI__.primitives.invoke;
+
+async function check_for_config() {
+    let config = await window.__TAURI__.fs.readTextFile("config.json");
+    if (config == "") {
+        windowLink.open("Setup");
+    }
+}
 
 new windowLink.Window("Setup", { url: "setup.html", width: 750, height: 500});
